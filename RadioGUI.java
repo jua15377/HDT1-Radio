d9f6fd0fe5b5dd07d847d52cdc8a418289cdab32
JonnathanJuarez <jua15377@uvg.edu.gt>
2016-07-10 20:18:30 -0600

    Cambio de dimensiones del marco. Antes 500x250, ahora 750x300

--- RadioGUI.java
+++ RadioGUI.java
@@ -5,7 +5,7 @@ import java.awt.*;
  * Esta clase arma la gui, llama al contenedor RadioPanel para mostrar en pantalla la aplicacion
  * @author Eric Mendoza 15002; Jonnathan Juarez 15377; Javier Jo 14343
  * @since 09/07/2016
- * @version 1.0
+ * @version 1.1
  */
 public class RadioGUI {
     public RadioGUI(){
@@ -15,7 +15,7 @@ public class RadioGUI {
         // Crear Marco
         JFrame marcoGUI = new JFrame();
         marcoGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        marcoGUI.setMinimumSize(new Dimension(500, 250));
+        marcoGUI.setMinimumSize(new Dimension(750, 300));
 
         // Lienzo para panel
         Container lienzo = marcoGUI.getContentPane();
