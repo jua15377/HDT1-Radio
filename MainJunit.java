60920e1f195d18dccb922b8a8a61c17b07122bb0
Eric <men15002@uvg.edu.gt>
2016-07-11 19:40:19 -0600

    Elimine la clase de carlos que se coló de más

--- MainJunit.java
+++ MainJunit.java
@@ -3,7 +3,7 @@ import org.junit.runner.JUnitCore;
 public class MainJunit {
 
     /**
-     * metodo obtenido de: https://www.youtube.com/watch?v=Exrp9vemFwY
+     * Metodo obtenido de: https://www.youtube.com/watch?v=Exrp9vemFwY
      *
      */
     public static void main(String[] args){
