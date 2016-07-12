e8a0f01ddf1310acc947c626876c9ae9eb0e81b0
Eric <men15002@uvg.edu.gt>
2016-07-11 19:37:45 -0600

    Elimine fotos de más

---
+++ IPOD.java
@@ -0,0 +1,59 @@
+/**
+ * Esta clase es la base del objeto radio, el cual implementa la interfaz RadioInterface
+ * @author Eric Mendoza 15002; Jonnathan Juarez 15377; Javier Jo 14343
+ * @since 09/07/2016
+ * @version 1.0
+ */
+public class IPOD implements RadioInterface{
+    //Atributos
+    private boolean encendido, frecuencia;
+    private double emisora;
+    private double[] frecsGuardadas = {87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9,87.9};
+
+    //Metodos
+    public IPOD(){
+        this.encendido = false;
+        this.frecuencia = true;
+        this.emisora = 87.9;
+    }
+
+    @Override
+    public void setEncendido(boolean estado) {
+        this.encendido = estado;
+    }
+
+    @Override
+    public void setFrecuencia(boolean frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    @Override
+    public void setEmisora(double emisora) {
+        this.emisora = emisora;
+    }
+
+    @Override
+    public void saveEmisora(int btn, double emisora) {
+        this.frecsGuardadas[btn-1] = emisora;
+    }
+
+    @Override
+    public double selectEmisora(int btn) {
+        return this.frecsGuardadas[btn-1];
+    }
+
+    @Override
+    public boolean getEncendido() {
+        return this.encendido;
+    }
+
+    @Override
+    public boolean getFrecuencia() {
+        return this.frecuencia;
+    }
+
+    @Override
+    public double getEmisora() {
+        return this.emisora;
+    }
+}
